57a195adba8e7949ce05b0ee3c4d349a123c0efb
emranaloul <emranaloul1992@hotmail.com>
2022-08-19 21:47:24 +0300

    try to deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,7 @@ module.exports = {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
   },
-  extends: [
-    'react-app',
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
+  
 
   plugins: ['react', 'react-hooks'],
   rules: {
